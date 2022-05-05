67c853c7d0ffe7402e04d957c48b04ef6b1e7399
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-05 15:41:58 +0200

    add one more object and indentation.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-let pokemonList = [{ name : 'Ivysaur' , height : 1 , type : ['monster', 'grass']}, 
-                   { name : 'Beedrill' , height : 1 , type : 'bug'} ];
+let pokemonList = [
+    { 
+        name : 'Ivysaur' , 
+        height : 1 , 
+        type : ['monster', 'grass']
+    }, 
+                   
+    { 
+        name : 'Beedrill' , 
+        height : 1 , 
+        type : 'bug'
+    },
+    {
+        name : 'Sandslash' , 
+        height : 1 , 
+        type : 'Field'
+    }
+];
