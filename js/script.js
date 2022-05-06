2b916f4ee6380fbc98e41436f20d2de47e5de96e
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-06 23:10:27 +0200

    Edited entry in pokemonList.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ let pokemonList = [
         height : 1 , 
         types : ['bug']
     },
+let pokemonList = 
+[
+    { name : 'Ivysaur' , height : 1 ,types : ['Monster, Grass']}, 
+    { name : 'Blastoise' , height : 1.6 , types : ['Monster, Water 1']},
+    { name : 'Sandslash' , height : 1 , types : ['Field']}
+];
+
     {
         name : 'Sandslash' , 
         height : 1 , 
