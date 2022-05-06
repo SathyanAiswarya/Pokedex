d47ab36c4581a4d759d1a0032b02e01a7839ce04
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-06 23:14:47 +0200

    add for loop to print Pokemon List and added if condition to check which pokemon is big.

--- js/script.js
+++ js/script.js
@@ -17,9 +17,18 @@ let pokemonList =
     { name : 'Sandslash' , height : 1 , types : ['Field']}
 ];
 
+for ( let i=0; i< pokemonList.length; i++ )
+{
+    document.write("<br>");
+    if( pokemonList[i].height > 1.5)
     {
         name : 'Sandslash' , 
         height : 1 , 
         types : ['Field']
+        document.write(`Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . Wow its s big Pokemon !! and types are "${pokemonList[i].types}"`);
     }
-];
+    else{
+        document.write(`Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . And types are "${pokemonList[i].types}"`);
+    }
+}
+
