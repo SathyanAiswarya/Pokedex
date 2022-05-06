f800553faa1012861ae68105c878b1e8f6f8384a
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-06 23:14:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-let pokemonList = [
-    { 
-        name : 'Ivysaur' , 
-        height : 1 , 
-        types : ['monster', 'grass']
-    }, 
-                   
-    { 
-        name : 'Beedrill' , 
-        height : 1 , 
-        types : ['bug']
-    },
 let pokemonList = 
 [
     { name : 'Ivysaur' , height : 1 ,types : ['Monster, Grass']}, 
@@ -22,9 +10,6 @@ for ( let i=0; i< pokemonList.length; i++ )
     document.write("<br>");
     if( pokemonList[i].height > 1.5)
     {
-        name : 'Sandslash' , 
-        height : 1 , 
-        types : ['Field']
         document.write(`Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . Wow its s big Pokemon !! and types are "${pokemonList[i].types}"`);
     }
     else{
