44534190ed61cbf706818bde970636f4757e5f43
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-08 12:46:24 +0200

    add HTML tag <p> within the string (if statement)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonList = 
 [
-    { name : 'Ivysaur' , height : 1 ,types : ['Monster, Grass']}, 
+    { name : 'Ivysaur' , height : 1 ,types : ['Monster  , Grass']}, 
     { name : 'Blastoise' , height : 1.6 , types : ['Monster, Water 1']},
     { name : 'Sandslash' , height : 1 , types : ['Field']}
 ];
@@ -10,10 +10,10 @@ for ( let i=0; i< pokemonList.length; i++ )
     document.write("<br>");
     if( pokemonList[i].height > 1.5)
     {
-        document.write(`Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . Wow its s big Pokemon !! and types are "${pokemonList[i].types}"`);
+        document.write(`<p> Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . Wow its s big Pokemon !! and types are "${pokemonList[i].types}" </p>`);
     }
     else{
-        document.write(`Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . And types are "${pokemonList[i].types}"`);
+        document.write(`<p> Name is "${pokemonList[i].name}" , its height is ${pokemonList[i].height} . And types are "${pokemonList[i].types}"</p>`);
     }
 }
 
