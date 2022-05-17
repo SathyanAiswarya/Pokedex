84bcee80b2f4a3bbd87e4288d6a462ae38b1e152
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-17 18:26:08 +0200

    add if conditionals for height of Pokemon.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
-let pokemonRepository =  (function (){
-    let pokemonList = 
-    [
-        { name : 'Ivysaur' , height : 1 ,types : ['Monster  , Grass']}, 
-        { name : 'Blastoise' , height : 1.6 , types : ['Monster, Water 1']},
-        { name : 'Sandslash' , height : 1 , types : ['Field']}
-    ];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Ivysaur", height: 1, types: ["Monster  , Grass"] },
+    { name: "Blastoise", height: 1.6, types: ["Monster, Water 1"] },
+    { name: "Sandslash", height: 1, types: ["Field"] },
+  ];
 
-    function getAll (){
-        return pokemonList;
-    }
+  function getAll() {
+    return pokemonList;
+  }
 
-    function add(newPokemon){
-        return pokemonList.push(newPokemon);
-    }
-    return{
-    getAll:  getAll,
-    add: add
-    };
-
+  function add(newPokemon) {
+    return pokemonList.push(newPokemon);
+  }
+  return {
+    getAll: getAll,
+    add: add,
+  };
 })();
 
-pokemonRepository.getAll().forEach((pokemonItems ) => (document.write (`<p> Name is "${pokemonItems.name}" , its height is ${pokemonItems.height} . And types are "${pokemonItems.types}"</p>`)));
+pokemonRepository.getAll()
+  .forEach((pokemonItem) =>{
+  if ( pokemonItem.height >1.5 ){
+    document.write(
+        `<p> Name is "${pokemonItem.name}" , Wow, its a big Pokemon!!,
+        its height is ${pokemonItem.height} . 
+        And types are "${pokemonItem.types}"</p>`);
+  }
+  else{
+    document.write(
+        `<p> Name is "${pokemonItem.name}" , 
+        its height is ${pokemonItem.height} . 
+        And types are "${pokemonItem.types}"</p>`); 
 
-
-
-
-
+  }
+  });
