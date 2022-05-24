a9c4890905f2619913475cc82cb2b9a32fec23a5
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-05-24 23:22:03 +0200

    add new function 'addListItem' and 'ShowDetails' to console Pokemon details.
    addListItem = added 'BUTTON'

--- js/script.js
+++ js/script.js
@@ -1,36 +1,50 @@
-let pokemonRepository = (function () {
+let pokemonRepository = ( () =>{
   let pokemonList = [
     { name: "Ivysaur", height: 1, types: ["Monster  , Grass"] },
     { name: "Blastoise", height: 1.6, types: ["Monster, Water 1"] },
     { name: "Sandslash", height: 1, types: ["Field"] },
   ];
 
-  function getAll() {
+  const getAll = () => {
     return pokemonList;
   }
 
-  function add(newPokemon) {
+  const add = (newPokemon) => {
     return pokemonList.push(newPokemon);
   }
+
+  const showDetails = (pokemon) => {
+     console.log(pokemon);
+ }
+
+  const addListItem = (pokemon) =>{
+            let pokemonList = document.querySelector('.pokemon_list');
+            let listItem = document.createElement('li');
+            let button = document.createElement ('button');
+            button.innerText = pokemon.name ;
+            button.classList.add('pokemon-name');
+            listItem.appendChild(button);
+            pokemonList.appendChild(listItem);
+
+            button.addEventListener( 'click' , ()=>{
+                showDetails(pokemon);
+            })
+  }
+
+  
+
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem,
   };
+
 })();
 
-pokemonRepository.getAll()
-  .forEach((pokemonItem) =>{
-  if ( pokemonItem.height >1.5 ){
-    document.write(
-        `<p> Name is "${pokemonItem.name}" , Wow, its a big Pokemon!!,
-        its height is ${pokemonItem.height} . 
-        And types are "${pokemonItem.types}"</p>`);
-  }
-  else{
-    document.write(
-        `<p> Name is "${pokemonItem.name}" , 
-        its height is ${pokemonItem.height} . 
-        And types are "${pokemonItem.types}"</p>`); 
+pokemonRepository.getAll().forEach((pokemon) =>{
+    pokemonRepository.addListItem(pokemon);
+});
 
-  }
-  });
+
+
+
